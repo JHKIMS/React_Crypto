05cfc3814dd666791bb97e999df66aa0f4c0bff9
JHKIMS <wjdgns5131@gmail.com>
2023-01-15 23:20:02 +0900

    CryptoTracker: PriceChart3+꾸미기

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -5,11 +5,11 @@ import ApexCharts from "react-apexcharts";
 interface IHistorical {
   time_open: number;
   time_close: number;
-  open: string
-  high: string
-  low: string
-  close: string
-  volume: string
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
   market_cap: number;
 }
 interface ChartProps {
@@ -17,52 +17,68 @@ interface ChartProps {
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>fetchCoinHistory(coinId));
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   return (
-  <div>
-    {isLoading? "Loading Chart...":( 
-  <ApexCharts 
-    type="line"
-    series={[
-        {
-            name: "ChartPrice",
-            data: data?.map((price) => parseFloat(price.close))??[]
-        },
-    ]}
-    options={{
-        theme:{
-            mode: "light"
-        },
-        chart:{
-            height: 300,
-            width:500,
-            background: "tranparent",
-        },
-        grid: { 
-            show: false,
-        },
-        stroke: {
-            curve: "smooth",
-            width: 5,
-        },
-        xaxis: {
-            axisBorder: {show: false},
-            axisTicks: {show: false},
-            labels: {show: false,}
-        },
-        yaxis: {
-            labels: {
-                style:{
-                    fontSize: '15px',
-                    fontWeight: 700,
-                }  
-            }
-        }
-    }}
-    />
-  )}
-</div>
-  )
+    <div>
+      {isLoading ? (
+        "Loading Chart..."
+      ) : (
+        <ApexCharts
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              background: "transparent",
+            },
+            grid: {show: false},
+            stroke: {
+              curve: "smooth",
+              width: 5
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+              type: "datetime",
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toISOString()
+              ),
+            },
+            yaxis: {
+              labels: {
+                style: {
+                  fontSize: "15px",
+                  fontWeight: 700,
+                },
+              },
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#44bd32"], stops: [0, 100] },
+            },
+            colors: ["#0984e3"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(3)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
