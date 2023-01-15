fdaaa1a8e79cbf8c69be5906daae42155110ce2c
JHKIMS <wjdgns5131@gmail.com>
2023-01-15 11:28:28 +0900

    CryptoTracker: Ts에 API데이터 타입 설명하기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -2,11 +2,74 @@ import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
-interface ParamsProp{
-    coinId:string;
+interface ParamsProp {
+  coinId: string;
 }
-interface RouteState{
-  name:string;
+interface RouteState {
+  name: string;
+}
+
+interface ITag {
+  coin_counter: number;
+  ico_counter: number;
+  id: string;
+  name: string;
+}
+
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  tags: ITag[]; // array같은 경우는 이렇게 처리해줘야 한다.
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
 }
 
 const Container = styled.div`
@@ -19,45 +82,47 @@ const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
- `;
- const Title = styled.h1`
+`;
+const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 const Loader = styled.span`
   text-align: center;
   display: block;
-`
+`;
 
-function Coin(){
-    const [loading, setLoading] = useState(true);
-    const {coinId} = useParams<ParamsProp>();
-    const {state} = useLocation<RouteState>();// useLocation : ReactRouterDom이 보내주는 것
-    const [info, setInfo] = useState({});
-    const [priceInfo, setPriceInfo] = useState({});
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<ParamsProp>();
+  const { state } = useLocation<RouteState>(); // useLocation : ReactRouterDom이 보내주는 것
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
-    useEffect(()=>{
-      (async()=>{
-        const infoData = await(
-          await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
-        ).json();
-        /* const response = await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      // console.log(infoData);
+      /* const response = await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
         const json = await response.json() 
         위의 코드와 동일하다.*/
-        const priceData = await(
-          await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
-        ).json();
-        setInfo(infoData);
-        setPriceInfo(priceData);
-      })()
-    }, [])
-    return (
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      // console.log(priceData);
+    })();
+  }, []);
+  return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading...(I can't find)"}</Title>
       </Header>
       {loading ? <Loader>Loading...</Loader> : null}
     </Container>
-    )
+  );
 }
 export default Coin;
