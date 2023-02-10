79a7af96d33740d161c5b75d52f3833a143cd244
JHKIMS <wjdgns5131@gmail.com>
2023-02-10 09:43:52 +0900

    CryptoTracker: 테마 색상 변경

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,17 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "black", 
-  accentColor: "#9c88ff",
+  bgColor: "#2C5364",
+  textColor: "black",
+  coinColor: "#2C5364",
+  accentColor: "#dfe6e9",
   borderColor: "1px solid #2c2d33"
 };
 
 export const lightTheme: DefaultTheme = {
-  bgColor: "#40739e",
-  textColor: "#fdcb6e", 
+  bgColor: "#f7f1e3",
+  textColor: "#fdcb6e",
+  coinColor: "#e1b12c", 
   accentColor: "#e1b12c",
   borderColor: "1px solid #eaeaea",
 };
